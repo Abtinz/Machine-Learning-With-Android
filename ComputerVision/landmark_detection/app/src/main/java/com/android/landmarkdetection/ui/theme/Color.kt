2a6bc4bcc8fb0ff4b3ea7f466619e74eb5880dd9
Abtinz <abtinzandi@gmail.com>
2024-10-21 13:02:17 +0330

    ComputerVision in Android

---
+++ ComputerVision/landmark_detection/app/src/main/java/com/android/landmarkdetection/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.android.landmarkdetection.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val primary = Color(0xFFDC5F00)
+val secondary = Color(0xFF373A40)
+val tertiary = Color(0xFF686D76)
